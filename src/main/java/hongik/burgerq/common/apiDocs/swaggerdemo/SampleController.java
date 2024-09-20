a89b10a28ce2d34f5fc697247a522b63ebd49686
Dompoo <dlckdrms0517@gmail.com>
2024-09-20 18:21:21 +0900

    chore: 패키지 구조 변경

--- src/main/java/hongik/burgerq/common/apiDocs/swaggerdemo/SampleController.java
+++ src/main/java/hongik/burgerq/common/apiDocs/swaggerdemo/SampleController.java
@@ -1,4 +1,4 @@
-package hongik.burgerq.swaggerdemo;
+package hongik.burgerq.common.apiDocs.swaggerdemo;
 
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
--- src/main/java/hongik/burgerq/common/apiDocs/swaggerdemo/SampleResponse.java
+++ src/main/java/hongik/burgerq/common/apiDocs/swaggerdemo/SampleResponse.java
@@ -1,4 +1,4 @@
-package hongik.burgerq.swaggerdemo;
+package hongik.burgerq.common.apiDocs.swaggerdemo;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
